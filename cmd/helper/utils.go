cb5086293290fdc3e7063742073ceade095b1d53
Kartik Shukla <kaartikshukla7120@gmail.com>
2025-03-13 16:48:39 +0530

    Added a function to implement haversine formula

--- cmd/helper/utils.go
+++ cmd/helper/utils.go
@@ -4,6 +4,7 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"math"
 
 	"golang.org/x/crypto/bcrypt"
 )
@@ -24,4 +25,18 @@ func HashPassword(password string) ([]byte, error) {
 		return nil, err
 	}
 	return hashedPassword, nil
+}
+
+// Haversine formula to calculate the distance between two points
+func Haversine(lat1, lon1, lat2, lon2 float64) float64 {
+	const R = 6371 // Earth radius in kilometers
+	dLat := (lat2 - lat1) * (math.Pi / 180.0)
+	dLon := (lon2 - lon1) * (math.Pi / 180.0)
+	lat1 = lat1 * (math.Pi / 180.0)
+	lat2 = lat2 * (math.Pi / 180.0)
+
+	a := math.Sin(dLat/2)*math.Sin(dLat/2) +
+		math.Sin(dLon/2)*math.Sin(dLon/2)*math.Cos(lat1)*math.Cos(lat2)
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+	return R * c
 }
